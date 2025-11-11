c09fcae961cddf45756b88bd2a452eda35c41e57
habibtuman993-droid <habibtuman993@gmail.com>
2025-11-10 17:59:55 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script sederhana untuk menambahkan interaksi, misalnya animasi atau event handler
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portofolio siap!');
+});
